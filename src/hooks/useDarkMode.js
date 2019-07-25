8ab64280abc1311b5f4efc54f4d44369aa658bec
Penny Tague <pmtague9@gmail.com>
2019-07-25 15:32:00 -0700

    WIP: hook 2 complete

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage.js';
 
 function useDarkMode() {
-    const []
+    const [value, setValue] = useLocalStorage('true');
+
+    useEffect( () => {
+        if (useLocalStorage === true) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.replace("dark-mode", "");
+        }
+    }), [value];
+
+    return [value, setValue];
 }
+
+export default useDarkMode;
