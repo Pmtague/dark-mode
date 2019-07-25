2e4df4147420c612a567a10a0b98ce0c7035bcd2
Penny Tague <pmtague9@gmail.com>
2019-07-25 15:46:31 -0700

    WIP: Trying to figure out the bugs for step 3 to work

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage.js';
 
 function useDarkMode() {
-    const [value, setValue] = useLocalStorage('true');
+    const [value, setValue] = useLocalStorage();
 
-    useEffect( () => {
+    useEffect(() => {
         if (useLocalStorage === true) {
-            document.body.classList.add("dark-mode");
+            return document.body.classList.add("dark-mode");
         } else {
-            document.body.classList.replace("dark-mode", "");
+            return document.body.classList.remove("dark-mode");
         }
-    }), [value];
+    }, [value]);
 
     return [value, setValue];
 }
